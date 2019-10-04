d926c62774d594a376fffc82654185099ed4ba1f
biewxw <gabrieldeoliveira.gdo23@gmail.com>
2019-10-04 11:52:43 -0300

    Backend updates

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const app = express()
+const cors = require('cors')
+const path = require('path')
+
 const routes = require('./routes')
+
+const app = express()
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-sysdr.mongodb.net/semana09?retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -13,7 +17,9 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-sysdr.mongodb.net/
 // req.params = Acessar route params (para edição, delete)
 // req.body = Acessar corpo da requisição (criação e edição)
 
+app.use(cors())
 app.use(express.json())
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes)
 
-app.listen(3333)
+app.listen(8080)
