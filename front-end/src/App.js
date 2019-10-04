3be8e1bbed04eeee8bb5eebb142ebe7d39adb2ae
biewxw <gabrieldeoliveira.gdo23@gmail.com>
2019-10-04 11:53:31 -0300

    New configs

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,42 +1,18 @@
-import React, { useState } from 'react'
-import api from './services/api'
+import React from 'react'
 import './App.css'
 
 import logo from './assets/logo.svg'
 
+import Routes from './routes'
+
 function App() {
-  const [email, setEmail] = useState('')
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    
-    // Email
-    console.log(email)
-  }
-
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
 
       {/* Main Content */}
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
-        </p>
-
-        {/* Formulário */}
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input 
-            type="email"
-            id="email"
-            placeholder="Seu melhor e-mail"
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-
-          <button type="submit" className="btn">Entrar</button>
-        </form>
+        <Routes />
       </div>
 
     </div>
